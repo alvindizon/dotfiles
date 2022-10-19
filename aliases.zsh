d2d2711ba1f77a943cd4f18ef418106630b1d4d2
Alvin Dizon <alvin.dizon91@gmail.com>
2022-10-19 14:20:41 +0800

    Add no-verify alias for force pushing
    Signed-off-by: Alvin Dizon <alvin.dizon91@gmail.com>

--- aliases.zsh
+++ aliases.zsh
@@ -1,3 +1,6 @@
 # copy this file to ~/.oh-my-zsh/custom/
 # git push origin -f
 alias ggpushf='ggpush -f'
+alias gcnm='git commit --no-verify -m'
+alias gcn!='git commit --amend --no-verify'
+alias ggpushfn='ggpush -f --no-verify'
